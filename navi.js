a3bd316eefe46368ab6f82620f5f604a6798a749
Navneeth7 <89082421+Navneeth7@users.noreply.github.com>
2021-08-23 23:00:05 +0530

    Add files via upload

---
+++ navi.js
@@ -0,0 +1,16 @@
+function makeRed(){
+    document.body.style.backgroundColor="red";
+
+}
+function makeBlue(){
+    document.body.style.backgroundColor="blue";
+    
+}
+function makeGreen(){
+    document.body.style.backgroundColor="green";
+    
+}
+function makeOrange(){
+    document.body.style.backgroundColor="orange";
+    
+}
